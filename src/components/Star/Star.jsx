55fc4776c57817767f3b88bff5808c96630b32b5
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2026-04-03 18:13:52 +0300

    go react

--- src/components/Star/Star.jsx
+++ src/components/Star/Star.jsx
@@ -9,7 +9,7 @@ export const Star = () => {
 
       <MyIcon/> 
       
-      <img src={square}/>
+      <img src={square} alt=""/>
 
       <svg width="76" height="76" viewBox="0 0 76 76" fill="none" xmlns="http://www.w3.org/2000/svg">
         <circle cx="38" cy="38" r="35.5" fill="#DA9C01" stroke="#FF0004" stroke-width="5"/>
