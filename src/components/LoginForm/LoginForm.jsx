40c2442f9f9379fee6383f907a940ffad9da651c
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2026-04-05 11:19:30 +0300

    go react

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -2,28 +2,71 @@ import { Component } from 'react';
 import css from './LoginForm.module.css';
 
 export class LoginForm extends Component{
- state = {};
+ state = {
+    email: '',
+    password: '',
+ };
+
+//  handleChangeEmail=(event)=>{
+//     console.log(event.target.value)
+//     this.setState({email: event.target.value})
+//  }
+
+//   handleChangePassword=(event)=>{
+//     console.log(event.target.value)
+//     this.setState({password: event.target.value})
+//  }
+
+  handleSubmit =(event)=>{
+    event.preventDefault();
+    console.log(this.state);
+    this.setState({ email: '', password: '',})
+    
+  }
+
+  handleChange =(event)=>{
+    // console.log(event.target.value)
+    const{name, value}=event.target
+    this.setState({[name]: value});
+  }
 
  render(){
+    const{email, password}=this.state
     return(
-        <form className={css.form}>
+        <form className={css.form}  onSubmit={this.handleSubmit}>
 
 
             <div className="">
                 <label className="">
                     <p>Email address</p>
-                    <input type="text" className="form-control"/>
+                    <input
+                    type="text"
+                    name="email"
+                    className="form-control"
+                    value={email}
+                    onChange={this.handleChange}
+                     />
                 </label>
             </div>
 
             <div className="">
                 <label className="">
                     <p>Password</p>
-                    <input type="text" className="form-control"/>
+                    <input
+                    type="text"
+                    name="password"
+                    value={password}
+                    className="form-control"
+                    onChange={this.handleChange}
+                    />
                 </label>
             </div>
 
-            <button className="" type="submit">
+            <button 
+            className=""
+            type="submit"
+           
+            >
                 Submit
            </button>     
 
--- src/components/Star/Star.jsx
+++ src/components/Star/Star.jsx
@@ -12,7 +12,7 @@ export const Star = () => {
       <img src={square} alt=""/>
 
       <svg width="76" height="76" viewBox="0 0 76 76" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="38" cy="38" r="35.5" fill="#DA9C01" stroke="#FF0004" stroke-width="5"/>
+        <circle cx="38" cy="38" r="35.5" fill="#DA9C01" stroke="#FF0004" strokeWidth="5"/>
       </svg>
 
     </div>
